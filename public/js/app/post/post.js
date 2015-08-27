3d43c0deabe70f957d52d92129aec51a2f9293c9
Vladislav Pavlov <ruspython@gmail.com>
2015-08-27 13:26:07 +0300

    Recent in category added

--- public/js/app/post/post.js
+++ public/js/app/post/post.js
@@ -2,16 +2,26 @@
 
 angular.module('mainApp.post', ['ngRoute'])
 
-.config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/post/:slug', {
-    templateUrl: '/views/post' + template_ext,
-    controller: 'PostCtrl',
-	controllerAs: 'postctrl'
-  });
-}])
+  .config(['$routeProvider', function ($routeProvider) {
+    $routeProvider.when('/post/:slug', {
+      templateUrl: '/views/post' + template_ext,
+      controller: 'PostCtrl'
+    });
+  }])
 
-.controller('PostCtrl', ['$routeParams', '$location', '$scope', 'Post', function($routeParams, $location, $scope, Post) {
-  Post.get({slug: $routeParams.slug}, function(post) {
-    $scope.post = post;
-  });
-}]);
+  .controller('PostCtrl', ['$routeParams', '$location', '$scope', 'Post', 'PostByCategory', function ($routeParams, $location, $scope, Post, PostByCategory) {
+    function activate() {
+      Post.get({slug: $routeParams.slug}, function (post) {
+        $scope.post = post;
+        
+        PostByCategory.query({key: post.categories[0].key}, function (recent_posts) {
+          $scope.recent_posts = recent_posts;
+        }, ErrorFn);
+      });
+    }
+    activate();
+
+    function ErrorFn(error) {
+      console.error(error);
+    }
+  }]);
--- routes/api/post_by_category.js
+++ routes/api/post_by_category.js
@@ -1,6 +1,6 @@
 var async = require('async'),
   keystone = require('keystone');
- 
+
 var Post = keystone.list('Post');
 var PostCategory = keystone.list('PostCategory');
 
@@ -9,13 +9,14 @@ var PostCategory = keystone.list('PostCategory');
  */
 exports.list = function(req, res) {
   PostCategory.model.findOne().where('key', req.params.key).exec(function(err, item) {
-    
+
     if (err) return res.apiError('database error', err);
     if (!item) return res.apiError('not found');
 
     var category = item;
-    
-    Post.model.find().populate('author categories').where('categories').in([category.id]).exec(function(err, items) {
+
+    // TODO: thumbnails
+    Post.model.find().populate('categories').where('categories').in([category.id]).sort({publishedDate: 'desc'}).limit(4).exec(function(err, items) {
       if (err) return res.apiError('database error', err);
       if (!items || items.length === 0) return res.apiError('not found');
 
