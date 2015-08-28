2a57099054d16035e609ebd216834e91419fbb7e
Vladislav Pavlov <ruspython@gmail.com>
2015-08-28 15:17:55 +0300

    Added infinite scroll for all and by-category lists

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,129 +1,138 @@
 var async = require('async'),
   keystone = require('keystone');
- 
-var Post = keystone.list('Post');
- 
+
+var Post = keystone.list('Post'),
+    PostCategory = keystone.list('PostCategory')
+  ;
+
 /**
  * List Posts
  */
-exports.list = function(req, res) {
-  Post.model.find().populate('categories').exec(function(err, items) {
-    
+exports.list = function (req, res) {
+  Post.model.find().populate('categories').exec(function (err, items) {
+
     if (err) return res.apiError('database error', err);
-    
+
     // res.apiResponse({
     //   posts: items
     // });
 
     res.apiResponse(items);
-    
+
   });
 };
- 
+
 /**
  * Get Post by ID
  */
-exports.get = function(req, res) {
-  Post.model.findOne().populate('categories').where('slug', req.params.slug).exec(function(err, item) {
+exports.get = function (req, res) {
+  Post.model.findOne().populate('categories').where('slug', req.params.slug).exec(function (err, item) {
     item.visits += 1;
     item.save();
     if (err) return res.apiError('database error', err);
     if (!item) return res.apiError('not found');
-    
+
     res.apiResponse(
       item
     );
-    
+
   });
 };
- 
- 
+
+
 /**
  * Create a Post
  */
-exports.create = function(req, res) {
-  
+exports.create = function (req, res) {
+
   var item = new Post.model(),
     data = (req.method == 'POST') ? req.body : req.query;
-  
-  item.getUpdateHandler(req).process(data, function(err) {
-    
+
+  item.getUpdateHandler(req).process(data, function (err) {
+
     if (err) return res.apiError('error', err);
-    
+
     res.apiResponse({
       post: item
     });
-    
+
   });
 };
- 
+
 /**
  * Get Post by ID
  */
-exports.update = function(req, res) {
-  Post.model.findById(req.params.id).exec(function(err, item) {
-    
+exports.update = function (req, res) {
+  Post.model.findById(req.params.id).exec(function (err, item) {
+
     if (err) return res.apiError('database error', err);
     if (!item) return res.apiError('not found');
-    
+
     var data = (req.method == 'POST') ? req.body : req.query;
-    
-    item.getUpdateHandler(req).process(data, function(err) {
-      
+
+    item.getUpdateHandler(req).process(data, function (err) {
+
       if (err) return res.apiError('create error', err);
-      
+
       res.apiResponse({
         post: item
       });
-      
+
     });
-    
+
   });
 };
- 
+
 /**
  * Delete Post by ID
  */
-exports.remove = function(req, res) {
+exports.remove = function (req, res) {
   Post.model.findById(req.params.id).exec(function (err, item) {
-    
+
     if (err) return res.apiError('database error', err);
     if (!item) return res.apiError('not found');
-    
+
     item.remove(function (err) {
       if (err) return res.apiError('database error', err);
-      
+
       return res.apiResponse({
         success: true
       });
     });
-    
+
   });
 };
 
 /**
  * List Posts
  */
-exports.all = function(req, res) {
+exports.all = function (req, res) {
   var skip = req.query.skip || 0,
-    limit = req.query.limit || 0;
+    limit = req.query.limit || 0,
+    categoryKey = req.query.categoryKey || "",
+    querySet = Post.model.find().populate('categories')
+    ;
 
+  if (categoryKey) {
+    PostCategory.model.findOne().where('key', categoryKey).exec(function (err, category) {
+      if (category) {
+        querySet.where('categories').in([category.id]);
+      }
+      finishQuerySet();
+    });
+  } else {
+    finishQuerySet();
+  }
 
-  Post.model
-    .find()
-    .populate('categories')
-    .skip(skip)
-    .limit(limit)
-    .exec(function(err, items) {
+  function finishQuerySet() {
+    querySet
+      .skip(skip)
+      .limit(limit)
+      .exec(function (err, items) {
+        if (err) return res.apiError('database error', err);
+        res.apiResponse(items);
 
-    if (err) return res.apiError('database error', err);
+      });
+  }
 
-    // res.apiResponse({
-    //   posts: items
-    // });
-    console.log(req.query);
-
-    res.apiResponse(items);
-
-  });
 };
