95f2ad021ac3911b94cfa7f5921a2d0cfc296257
Vladislav <ruspython@gmail.com>
2015-09-01 18:58:09 +0300

    Ads fixed

--- public/js/app/services.js
+++ public/js/app/services.js
@@ -92,7 +92,7 @@ function PostUtils() {
     try {
       $headers.each(function (index) {
         if (index === 3) throw BreakException; 
-					$(this).after('<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script><ins class="adsbygoogle" style="display:inline-block;width:336px;height:280px" data-ad-client="ca-pub-3833845702235676" data-ad-slot="5302179685"></ins> <script> (adsbygoogle = window.adsbygoogle || []).push({}); </script>');
+					$(this).after('<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script><ins class="adsbygoogle" style="display:inline-block;width:336px;height:280px" data-ad-client="ca-pub-3833845702235676" data-ad-slot="'+adSlots[index]+'"></ins> <script> (adsbygoogle = window.adsbygoogle || []).push({}); </script>');
       });
     } catch (e) {
       if (e !== BreakException) throw e;
